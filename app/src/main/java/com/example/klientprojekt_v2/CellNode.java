ab4028f25c777ac219463e1d9d4f472c750b8337
streiitan <jenniferstreit@hotmail.com>
2022-02-15 16:00:56 +0100

    Clean

--- app/src/main/java/com/example/klientprojekt_v2/CellNode.java
+++ app/src/main/java/com/example/klientprojekt_v2/CellNode.java
@@ -1,5 +1,15 @@
 package com.example.klientprojekt_v2;
 
+
+/**
+ * Ett spelbräde bör ha koordinaterna 4x8. Även om de spelas på 8x8 men eftersom de endast är de
+ * svarta rutorna som spelas på kan man minska till 4x8
+ */
 public class CellNode {
+
+    public CellNode() {
+
+    }
+
 
 }
