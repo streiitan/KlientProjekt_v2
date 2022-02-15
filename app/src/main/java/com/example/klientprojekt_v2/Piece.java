dcf94170c57675291a724b74860716f1863ba0fc
streiitan <jenniferstreit@hotmail.com>
2022-02-15 14:09:43 +0100

    Start

--- app/src/main/java/com/example/klientprojekt_v2/Piece.java
+++ app/src/main/java/com/example/klientprojekt_v2/Piece.java
@@ -1,4 +1,17 @@
 package com.example.klientprojekt_v2;
 
 public class Piece {
+    private boolean king;
+
+    public Piece() {
+        king = false;
+    }
+
+    public boolean isKing() {
+        return king;
+    }
+
+    public void setKing() {
+        king = true;
+    }
 }
