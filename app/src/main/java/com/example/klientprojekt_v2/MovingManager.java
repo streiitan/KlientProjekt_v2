814705be880b7145055f92180e67795482f7ef88
streiitan <jenniferstreit@hotmail.com>
2022-02-16 13:05:30 +0100

    Utveckling av flytta

--- app/src/main/java/com/example/klientprojekt_v2/MovingManager.java
+++ app/src/main/java/com/example/klientprojekt_v2/MovingManager.java
@@ -1,5 +1,7 @@
 package com.example.klientprojekt_v2;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 
 public class MovingManager {
@@ -10,7 +12,6 @@ public class MovingManager {
     private final int Y_MAX = 8;
 
     private ArrayList<CellNode> cellNodeList;
-
 
     /**
      * Sätter upp CellNoder för hela brädet
@@ -49,11 +50,36 @@ public class MovingManager {
         int markedCell = y * X_MAX + x;
         CellNode cell = cellNodeList.get(markedCell);
         if (cell.hasPiece()) {
-            if (cell.getPieceColor() == Color.RED){
-
+            if (cell.getPieceColor() == Color.RED) {
+                if (y % 2 == 0 ) {
+                    CellNode rightCell = cellNodeList.get(markedCell + X_MAX);
+                    if (markedCell % 4 != 0) {
+                        CellNode leftCell = cellNodeList.get(markedCell + X_MAX -1);
+                    }
+                } else {
+                    CellNode leftCell = cellNodeList.get(markedCell -X_MAX);
+                    if (markedCell % 4 != 3) {
+                        CellNode rightCell = cellNodeList.get(markedCell + X_MAX +1);
+                    }
+                }
+                if (cell.pieceIsKing()) {
+                    //TODO lägg till bakåt
+                }
             } else {
+                if (y % 2 == 0) {
+                    CellNode rightCell = cellNodeList.get(markedCell - X_MAX);
+                    if (markedCell % 4 != 0) {
+                        CellNode leftCell = cellNodeList.get(markedCell - X_MAX -1);
+                    }
+                } else {
 
-            }
+                }
+            };
         }
+    }
+
+    public void cellMarked(int i, CellNode cell) {
+        int markedCell = i;
+
     }
 }
